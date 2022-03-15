d003861d5a153f598f47a74b43e6d777d8ebefe1
ANDREW-ALUNO <101295825+ANDREW-ALUNO@users.noreply.github.com>
2022-03-15 14:01:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup(){
   createCanvas(400,400);
   background("blue");
 
-  // Plano de fundo móvel
+
   sea=createSprite(400,200);
   sea.addImage(seaImg);
   sea.velocityX = -5;
@@ -29,7 +29,7 @@ function draw() {
   sea.velocityX = -3;
 
   
-  //código para redefinir o plano de fundo
+  
   if(sea.x < 0){
     sea.x = sea.width/8;
   }
